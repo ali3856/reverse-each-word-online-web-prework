ca49cb81ecb3d241f880d59d031045f7d1225315
witty-banner-1950 <asimsali95@gmail.com>
2019-06-13 20:01:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,12 @@ def reverse_each_word (string)
   end
   reversed.join(" ")
 end
+
+def reverse_each_word (string)
+  split_string = string.split(" ")
+  reversed = []
+  split_string.collect do |word|
+    reversed << word.reverse
+  end
+  reversed.join(" ")
+end
