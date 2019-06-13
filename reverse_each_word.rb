d7c04ad0aa4eaeafb6bfd0bcdc562804484f1f50
witty-banner-1950 <asimsali95@gmail.com>
2019-06-13 19:59:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word (string)
   split_string = string.split(" ")
   reversed = []
   split_string.each do |word|
-    revered << word.reverse
+    reversed << word.reverse
   end
   reversed.join(" ") 
 end
